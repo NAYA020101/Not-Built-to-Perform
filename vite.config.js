8b8a04e4261331d7ada8d6aa77e1d36cf9bcf875
NAYA020101 <1123504524@qq.com>
2026-06-23 12:08:35 +0800

    Clean all home page jump links

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
       input: {
         main: './index.html',
         menu: './menu.html',
-        home: './pages/home.html',
         about: './pages/about.html',
         archive: './pages/archive.html',
         lab: './pages/lab.html',
